ad355bdb197bed6579573c44749d0e66e387a49c
Azic-rello <github1akk2009@gmail.com>
2025-12-12 23:09:57 +0500

    updates

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,13 +1,13 @@
 import Navbar from "../components/Navbar";
+import ContactSection from "../components/contact/ContactSection";
 import Hero from "../components/contact/Hero";
-import Contact from "../components/contact/Contact";
 
 const Contact = () => {
   return (
     <>
       <Navbar />
       <Hero />
-      <Contact />
+      <ContactSection />
     </>
   );
 };

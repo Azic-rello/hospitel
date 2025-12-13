4327aee77ae0238b8054db7c98ae11ea32072b01
Azic-rello <github1akk2009@gmail.com>
2025-12-13 20:51:23 +0500

    marjonanikini qoshdim

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,17 +1,18 @@
-import React from "react";
-
- function Info() {
+function Info() {
   return (
     <div className="relative w-full h-[500px] flex items-center justify-center text-center">
-      <div 
-        className="absolute inset-0 bg-cover bg-center"
-        
-      > <img src="/public/assets/3872cf531b67c6202c38dc53126a3cc49440d867.jpg" alt="xatolik" className="w-full h-[600px]"/> </div>
+      <div className="absolute inset-0 bg-cover bg-center">
+        {" "}
+        <img
+          src="/public/assets/3872cf531b67c6202c38dc53126a3cc49440d867.jpg"
+          alt="xatolik"
+          className="w-full h-[600px]"
+        />{" "}
+      </div>
 
       <div className="absolute inset-0 bg-blue-900/60 w-full h-[600px]"></div>
 
       <div className="relative max-w-4xl px-6 text-white">
-        
         <div className="text-6xl font-bold mb-6">“</div>
 
         <p className="text-lg md:text-xl mb-6 leading-relaxed">
@@ -21,7 +22,7 @@ import React from "react";
           faucibus porttitor enim et.
         </p>
 
-        <div className="w-32 h-[2px] bg-white/60 mx-auto mb-4"></div>
+        <div className="w-32 h-[2.1px] bg-white/60 mx-auto mb-4"></div>
 
         <h3 className="text-2xl font-semibold">John Doe</h3>
 
@@ -34,4 +35,4 @@ import React from "react";
     </div>
   );
 }
-export default Info
+export default Info;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,24 @@
+import siteLogo from "../../public/assets/hospitelLogo.png";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="w-full bg-[#2d076e] h-[81px] flex items-center justify-around">
+      <img className="w-30" src={siteLogo} alt="jpg" />
+      <ul>
+        <li>Home</li>
+        <li>About us</li>
+        <li>Services</li>
+        <li>Doctors</li>
+        <li>News</li>
+        <li>Contact</li>
+      </ul>
+      <div>
+        <span>
+          <IoSearch />
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
